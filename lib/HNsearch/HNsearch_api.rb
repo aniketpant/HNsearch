83c0d69a165c1a35c6874f87968378f85b8e9a3c
Aniket Pant <me@aniketpant.com>
2013-04-03 21:26:27 +0530

    CLI done using Thor
    Signed-off-by: Aniket Pant <me@aniketpant.com>

--- lib/HNsearch/HNsearch_api.rb
+++ lib/HNsearch/HNsearch_api.rb
@@ -2,17 +2,17 @@ require "nestful"
 
 module HNsearch
   class HNsearchAPI
-    def initialize
+    def self.initialize
       @client = "http://api.thriftdb.com/api.hnsearch.com/"
     end
 
-    def query_users(query)
-      api_response = Nestful.get @client + "users/_search", :q => query
+    def self.query_users(query)
+      api_response = Nestful.get "#{@client}users/_search?q=#{query}"
       return api_response
     end
 
-    def query_items(query)
-      api_response = Nestful.get @client + "items/_search", :q => query
+    def self.query_items(query)
+      api_response = Nestful.get "#{@client}items/_search?q=#{query}"
       return api_response
     end
   end
--- lib/HNsearch/cli.rb
+++ lib/HNsearch/cli.rb
@@ -1,7 +1,20 @@
+require "HNsearch"
 require "thor"
 
 module HNsearch
   class CLI < Thor
+    package_name "HNsearch"
+    map "-u" => :users
+    map "-i" => :items
 
+    desc "users [QUERY]", "list all users"
+    def users(query="")
+      HNsearch::users(query)
+    end
+
+    desc "items [QUERY]", "list all items"
+    def items(query="")
+      HNsearch::items(query)
+    end
   end
 end
