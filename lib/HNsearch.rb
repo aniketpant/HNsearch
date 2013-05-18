3d2fba2d9b4d90686eef50c73e9028b9f9722bde
Aniket Pant <me@aniketpant.com>
2013-05-18 21:04:17 +0530

    Add docs
    Signed-off-by: Aniket Pant <me@aniketpant.com>

--- lib/HNsearch.rb
+++ lib/HNsearch.rb
@@ -1,20 +1,57 @@
 require "HNsearch/version"
 require "HNsearch/HNsearch_api"
 
+# The driver
 module HNsearch
+  # Use this to search for users
+
+  # Example:
+  #   >> HNsearch.users("aniketpant")
+  #   => Returns JSON object of the result and then runs print_users
+  #
+  # Arguments:
+  #   query: (String)
+
   def self.users(query)
     result = HNsearchAPI.new.query_users(query)
     print_users filter(result)
   end
+
+  # Use this to search for items
+
+  # Example:
+  #   >> HNsearch.items("awesome")
+  #   => Returns JSON object of the result and then runs print_items
+  #
+  # Arguments:
+  #   query: (String)
 
   def self.items(query)
     result = HNsearchAPI.new.query_items(query)
     print_items filter(result)
   end
 
+  # This function fetches the results part of the JSON object
+
+  # Example:
+  #   >> HNsearch.filter(some_json_object)
+  #   => Returns result key-value from the JSON object
+  #
+  # Arguments:
+  #   result: (JSON object)
+
   def self.filter(result)
     return result.fetch("results")
   end
+
+  # This function fetches the results part of the JSON object
+
+  # Example:
+  #   >> HNsearch.filter(some_json_object)
+  #   => Returns result key-value from the JSON object
+  #
+  # Arguments:
+  #   result: (JSON object)
 
   def self.print_users(result)
     result.each {
